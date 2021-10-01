d01ec119af654f73c670793274453e6f9a1c96d6
ZZZping <475377765@qq.com>
2021-10-01 13:54:07 +0800

    add sicp-pict package

--- ch02/ch02.rkt
+++ ch02/ch02.rkt
@@ -409,10 +409,38 @@
 ; (product-of-squares-of-odd-elements (list 1 2 3 4 5 6 7))
 
 ;;A PICTURE LANGUAGR
+(#%require sicp-pict)
+; (paint einstein)
 ; (define wave2 (beside wave (flip-vert wave)))
 ; (define wave4 (below wave2 wave2))
 
 (define (flipped-pairs painter)
   (let ((painter2 (beside painter (flip-vert painter))))
     (below painter2 painter2)))
-(define wave4 (flipped-pairs wave))
+; (define wave4 (flipped-pairs wave))
+
+(define (right-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (right-split painter (- n 1))))
+        (beside painter (below smaller smaller)))))
+(define (up-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (up-split painter (- n 1))))
+        (below painter (beside smaller smaller)))))
+(define (corner-split painter n)
+  (if (= n 0) painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1))))
+        (let ((top-left (beside up up))
+              (bottom-right (below right right))
+              (corner (corner-split painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
+(define (square-limit painter n)
+  (let ((quarter (corner-split painter n)))
+    (let ((half (beside (flip-horiz quarter) quarter)))
+      (below (filp-vert half) half))))
+;test
+; (paint (right-split einstein 3))
